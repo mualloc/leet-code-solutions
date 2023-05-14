63d99df612455926ed20a97270f609d603629f2d
mualloc <kavurmasucuk@gmail.com>
2023-05-14 18:28:00 +0300

    Add Catalan Number solution to Generate Parentheses
    https://leetcode.com/problems/generate-parentheses/solutions/10127/an-iterative-method/

--- Patterns/Generate-Parentheses.cpp
+++ Patterns/Generate-Parentheses.cpp
@@ -1,4 +1,25 @@
+// Catalan number: https://leetcode.com/problems/generate-parentheses/solutions/10127/an-iterative-method/
 class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<vector<string>> dp;
+        dp.push_back({""});
+
+        for(int i = 1; i <= n; ++i){
+            dp.push_back({});
+            for(int j = 0; j < i; ++j){
+                for(const auto& left : dp[j]){
+                    for(const auto& right : dp[i - 1 - j]){
+                        dp[i].push_back('(' + left + ')' + right);
+                    }
+                }
+            }
+        }
+        return dp.back();
+    }
+};
+
+class Solution2 {
 public:
     vector<string> generateParenthesis(int n) {
         vector<string> result;
